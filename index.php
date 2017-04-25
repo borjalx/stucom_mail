597dd5eb1566c0b2b272be3a55116339a11e9564
borja <borja@localhost>
2017-04-25 16:22:25 +0200

    Working at home

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="login.php"> Iniciar sesión </a>
+        <a href="crear_u.php"> Crear usuario<a/>
+    </body>
+</html>
